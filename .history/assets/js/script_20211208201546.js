01e42b83935f6ffca9f884ebde09b2a5529c6151
naomi <naomilounsbury@hotmail.com>
2021-12-08 20:52:52 -0800

    woohoo, got one of the fetches to work and dynamically generate data onto the page, oh la la we're so smart

--- .history/assets/js/script_20211208201546.js
+++ .history/assets/js/script_20211208201546.js
@@ -1,7 +1,6 @@
 // one var for the btn and one for the place on the page we want to put our events 
 var button = document.querySelector(".form")
 var eventList = document.querySelector(".event-list")
-// this one if to grab whatever is input into our input box
 var cityInput = document.querySelector("#city-input")
 //creating a function to fetch the open weather api, I used this one because the search parameters are citys not longitude and latitude
 var getWeather = function () {
@@ -11,16 +10,13 @@ var getWeather = function () {
             return response.json();
         })
         .then(function (data) {
-            console.log(data)
-            //TODO: get the weather on the page 
         })
     }
-// this function is to grab the event and put them on the page 
+
     var getEvent = function (event) {
         console.log(event.target)
         console.log(cityInput.value)
         event.preventDefault()
-        //made the url dynamis so it worked
         var eventbriteUrl = `https://app.ticketmaster.com/discovery/v2/events.json?city=${cityInput.value}&apikey=E2I7ya5FHRR8ZB0ACIGyv02xtzcbvJSw`
         fetch(eventbriteUrl)
             .then(function (response) {
@@ -28,13 +24,12 @@ var getWeather = function () {
             })
             .then(function (data) {
                 console.log(data)
-                //the for loop is so it runs through all the urls of the event and puts them on the page
-                for (var i = 0; i < data._embedded.events.length; i++) {
+                for (var i = 0; i < data.length; i++) {
                     // Create a list element
                     var listItem = document.createElement('li');
 
-                    // so here we are using innerhtml because intertext and textcontent don't allow us to add html elements in with dynamically generated titles and links
-                    listItem.innerHTML = `<a href=${data._embedded.events[i].url}>${data._embedded.events[i].name}</a>`;
+                    // Set the text of the list element to the JSON response's .html_url property
+                    listItem.textContent = data[i].html_url;
                     eventList.appendChild(listItem);
                     
                 }
@@ -43,12 +38,5 @@ var getWeather = function () {
     }
 
 
-//need one function to call both at the same time
-    var doBoth = function(){
-        getEvent()
-        getWeather()
-    }
 
-
-
-button.addEventListener('submit', doBoth)
+button.addEventListener('submit', getEvent)
