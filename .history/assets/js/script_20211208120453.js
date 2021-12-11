807e0c1f8786dc2b4442e63ddbca8953fe931efe
naomi <naomilounsbury@hotmail.com>
2021-12-11 13:59:14 -0800

    deleted history

--- .history/assets/js/script_20211208120453.js
+++
@@ -1,2 +0,0 @@
-var requestEventbriteUrl =
-var requestOpenWeatherUrl
--- .history/assets/js/script_20211208140844.js
+++
@@ -1,3 +0,0 @@
-var requestEventbriteUrl =
-var OpenWeatherUrl = https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=82b88905657c227b366aeed2a3762dff
-var EventbriteUrl = https://www.eventbriteapi.com/v3/users/me/?token=LMIDU5RTJRH3MM6FEHRV
