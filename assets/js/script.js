428741d6adc2cf9574b21eca9c074e288082d77a
naomi <naomilounsbury@hotmail.com>
2021-12-14 16:00:41 -0800

    I added a function to show the hotel data. we still need to organize the hotel data but I just realized we dont need to click an event to get the hotel data because its all in the same freaking city. They should probably show up side by side in the same click.

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,7 @@ var getWeather = function () {
         })
         .then(function (data) {
             console.log(data)
-            //TODO: get the weather on the page 
+            // got the current weather on the page was going to make the weather for the day of the event appear but that is an api we'd need to pay for so f that.
             displayWeather(data)
         })
 
@@ -58,6 +58,7 @@ var getEvent = function (event) {
                 // so here we are using innerhtml because intertext and textcontent don't allow us to add html elements in with dynamically generated titles and links
                 // listItem.innerHTML = `<a href=${data._embedded.events[i].url}>${data._embedded.events[i].name}</a>`;
                 listItem.textContent = data._embedded.events[i].name
+                //so I changed this because we don't want to get the url to buy tickets yet, right now we just want the data for the hotels 
                 listItem.addEventListener("click", getHotelLocation)
                 eventList.appendChild(listItem);
 
@@ -66,7 +67,9 @@ var getEvent = function (event) {
         });
 }
 // fetch("https://hotels-com-provider.p.rapidapi.com/v1/hotels/search?checkin_date=2022-03-26&checkout_date=2022-03-27&sort_order=STAR_RATING_HIGHEST_FIRST&destination_id=1708350&adults_number=1&locale=en_US&currency=USD&children_ages=4%2C0%2C15&price_min=10&star_rating_ids=3%2C4%2C5&accommodation_ids=20%2C8%2C15%2C5%2C1&price_max=500&page_number=1&theme_ids=14%2C27%2C25&amenity_ids=527%2C2063&guest_rating_min=4", {
-
+// so I need a function just to call the api to get the destinationid because one
+//api searches by location and one api searches by destination id and I don't even know anymore what I did
+//because it was basically switching things around until they worked
 var getHotelLocation = function(){
     var hotelLocationUrl = `https://hotels-com-provider.p.rapidapi.com/v1/destinations/search?query=${cityInput.value}&currency=USD&locale=en_US`
     fetch(hotelLocationUrl, {
@@ -81,6 +84,7 @@ var getHotelLocation = function(){
 })
 .then(function(data){
     console.log(data)
+    //had to call get hotels here because I called gethotellocation with the onclick
     getHotels(data)
 
 })
@@ -88,8 +92,9 @@ var getHotelLocation = function(){
 // 	console.error(err);
 }
 
-
+// this function is the one to get the name of the hotels 
 var getHotels = function(data){
+    //seriously, just to get the destination id, I had to dig
     var destinationId = data.suggestions[0].entities[0].destinationId
 
 
@@ -107,11 +112,41 @@ var getHotels = function(data){
 })
 .then(function(data){
     console.log(data)
+    showHotels(data)
+
 
 // .catch(err => {
 // 	console.error(err);
 });
 }
+var hotels = document.querySelector(".hotel-motel")
+//make a function to get the data from the hotels on the screen
+var showHotels = function(data){
+    var hotelResults = data.data.body.searchResults.results[0]
+    for (var i = 0; i < hotelResults.name.length; i++) {
+        // Create a list element
+        
+        var listItem = document.createElement('li');
+        var hotelName = document.createElement('p')
+        var hotelAddress = document.createElement('p')
+        var hotelStar = document.createElement('p')
+        
+
+        // so here we are using innerhtml because intertext and textcontent don't allow us to add html elements in with dynamically generated titles and links
+        // listItem.innerHTML = `<a href=${data._embedded.events[i].url}>${data._embedded.events[i].name}</a>`;
+
+        hotelName.innerHTML = `<a href=${hotelResults.optimizedThumbUrls.srpDesktop}>${hotelResults.name}</a>`
+        //just got object object will have to stringify
+        hotelAddress.textContent = hotelResults.address
+        hotelStar.textContent = hotelResults.starrating
+
+        
+        
+        hotels.appendChild(listItem);
+        listItem.append(hotelName, hotelAddress, hotelStar)
+    }
+}
+
 
 //need one function to call both at the same time
 var doBoth = function (event) {
@@ -121,5 +156,5 @@ var doBoth = function (event) {
 }
 
 
-
+//this button is just for  the weather and the events so do both isn't a great name for it
 button.addEventListener('submit', doBoth)
